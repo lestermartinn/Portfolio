8a47b09390cb5fa856127f12a9e7c1e4494bc81e
lestermartinn <lesterdanielmartin@gmail.com>
2025-06-08 04:46:31 -0400

    minor bug fixes & changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Navbar />
           <ClientWrapper>{children}</ClientWrapper>
         </ThemeProvider>
